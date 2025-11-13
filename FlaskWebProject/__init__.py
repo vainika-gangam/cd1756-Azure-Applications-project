f032c36591d348fe029ec851d8fb4df4f3990f86
vainika-gangam <gangamvynikareddy@gmail.com>
2025-11-13 09:55:53 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,20 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+# TODO: Add any logging levels and handlers with app.logger
+app.logger.setLevel(logging.INFO)
+
+# stream handler so logs show up in Azure Log Stream
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
+)
+stream_handler.setFormatter(formatter)
+
+app.logger.addHandler(stream_handler)
+
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
